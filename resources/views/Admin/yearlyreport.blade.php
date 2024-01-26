d57e5c7587ed9183364b7b846c4fc72f66444d9f
Happy Coding <mmhizham@cut.ac.zw>
2024-01-26 09:43:12 +0200

    yearly report

--- resources/views/Admin/yearlyreport.blade.php
+++ resources/views/Admin/yearlyreport.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
     <div class="card mt-3  ">
         <div class="card-header">
-            <h3 class="card-title">QUARTERLY PERFOMANCE REPORTS</h3>
+            <h3 class="card-title">YEARLY PERFOMANCE REPORTS</h3>
             <div class="card-tools">
                 <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                 </button>
